a0f29a18a883890d69a9d038333c7c2f6c204e9d
Zayn <zeinsoykat@gmail.com>
2026-09-17 11:45:25 +0300

    again 25 super easy lmao

---
+++ 800/Again Twenty Five!.py
@@ -0,0 +1,15 @@
+"""
+
+5, 1 = 5
+5, 2 = 25
+5, 3 = 125
+5, 4 = 625
+5, 5 = 3125
+5, 6 =  15625
+
+alright so lol we all can see that its always gonna end with 25
+
+"""
+
+number = int(input())
+print(25)
